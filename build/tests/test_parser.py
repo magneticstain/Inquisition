0fe7f7f51384a8604d96b466439e2f3f2d4502d9
Josh Carlson <magneticstain@gmail.com>
2017-04-29 17:46:40 -0400

    Add Unit Tests for Stats Logic in Parser()

--- build/tests/test_parser.py
+++ build/tests/test_parser.py
@@ -47,7 +47,7 @@ class AnatomizeTestCase(unittest.TestCase):
         statKey = '2_fake_apache_logs'
         statName = 'total_logs_processed'
 
-        # set log
+        # set stat
         self.parser.updateStatInLogDB(statName=statName, statKey=statKey, newVal=0, action='set')
         currentNumLogs = int(self.parser.logDbHandle.hget('stats:parser:' + statKey, statName).decode('utf-8'))
         self.assertEqual(currentNumLogs, 0)
@@ -99,6 +99,75 @@ class AnatomizeTestCase(unittest.TestCase):
             self.assertTrue(False)
         except ValueError:
             self.assertTrue(True)
+
+    def test_avgStat(self):
+        statName = 'average_log_length'
+        initialVal = 1
+        newVal = 5
+        numValsInSet = 2
+
+        # set stat
+        self.parser.stats[statName] = initialVal
+
+        self.parser.avgStat(statKey=statName, initialVal=initialVal, newVal=newVal, numValsInSet=numValsInSet)
+
+        calcAvg = self.parser.stats[statName]
+        actualAvg = (initialVal + newVal) / 2
+
+        self.assertEqual(calcAvg, actualAvg)
+
+    def test_avgStat_strictInvalidKey(self):
+        statName = 'average_log_length'
+        initialVal = 1
+        newVal = 5
+        numValsInSet = 2
+
+        del self.parser.stats[statName]
+
+        try:
+            self.parser.avgStat(statKey=statName, initialVal=initialVal, newVal=newVal, numValsInSet=numValsInSet
+                                , strict=True)
+
+            # if we get here, we didn't get to where we expected; considered failure
+            self.assertTrue(False)
+        except IndexError:
+            self.assertTrue(True)
+
+    def test_avgStat_checkDbVals(self):
+        statKey = '2_fake_apache_logs'
+        statName = 'average_log_length'
+        initialVal = 1
+        newVal = 5
+        numValsInSet = 2
+
+        # set stat
+        self.parser.stats[statName] = initialVal
+
+        self.parser.avgStat(statKey=statName, initialVal=initialVal, newVal=newVal, numValsInSet=numValsInSet, storeInDb=True)
+
+        inMemAvg = self.parser.stats[statName]
+        actualAvg = (initialVal + newVal) / 2
+
+        # check val in db
+        logDbAvgStat = float(self.parser.logDbHandle.hget('stats:parser:' + statKey, statName).decode('utf-8'))
+
+        self.assertEqual(inMemAvg, logDbAvgStat)
+        self.assertEqual(actualAvg, logDbAvgStat)
+
+    def test_resetParserStats_specificStatAndVal(self):
+        statKey = '2_fake_apache_logs'
+        statName = 'total_logs_processed'
+
+        # set stat
+        self.parser.stats[statName] = 0
+
+        self.parser.resetParserStats(statType='total_logs_processed', statData=1)
+
+        self.assertEqual(self.parser.stats[statName], 1)
+
+        # get val in db
+        logDbStatVal = int(self.parser.logDbHandle.hget('stats:parser:' + statKey, statName).decode('utf-8'))
+        self.assertEqual(logDbStatVal, 1)
 
 if __name__ == '__main__':
     unittest.main()
