89e251d615f69eca72f83e48bede76f6ae3fffcb
Josh Carlson <magneticstain@gmail.com>
2017-05-04 18:40:11 -0400

    Update Build Process to Utilize Install Script
    Completes: Issue #34

--- install/install.sh
+++ install/install.sh
@@ -6,6 +6,14 @@
 # v1.0
 #
 
+# check if this is a build or not (build don't require MySQL passwords
+if [ "$1" == "--is-build" ]
+then
+    BUILD_FLAG=1
+else
+    BUILD_FLAG=0
+fi
+
 APP_DIR='/opt/inquisition/'
 LOG_DIR='/var/log/inquisition/'
 
@@ -22,12 +30,22 @@ mkdir $LOG_DIR > /dev/null 2>&1
 rsync -av --exclude 'build' --exclude 'install' --exclude '.travis.yml' ../* $APP_DIR || exit 1
 
 # provision db
-echo "Initializing database..."
-mysql -u root -p -e "CREATE DATABASE inquisition"
-echo "Creating DB service account..."
-mysql -u root -p -e "CREATE USER inquisition@'localhost' IDENTIFIED BY ''; GRANT SELECT,INSERT,UPDATE,DELETE ON inquisition.* TO inquisition@'localhost'; FLUSH PRIVILEGES"
-echo "Import table schema..."
-mysql -u root -p  inquisition < ./src/inquisition.sql || exit 1
+if $BUILD_FLAG
+then
+    echo "Initializing database..."
+    mysql -u root -e "CREATE DATABASE inquisition"
+    echo "Creating DB service account..."
+    mysql -u root -e "CREATE USER inquisition@'localhost' IDENTIFIED BY ''; GRANT SELECT,INSERT,UPDATE,DELETE ON inquisition.* TO inquisition@'localhost'; FLUSH PRIVILEGES"
+    echo "Import table schema..."
+    mysql -u root inquisition < ./src/inquisition.sql || exit 1
+else
+    echo "Initializing database..."
+    mysql -u root -p -e "CREATE DATABASE inquisition"
+    echo "Creating DB service account..."
+    mysql -u root -p -e "CREATE USER inquisition@'localhost' IDENTIFIED BY ''; GRANT SELECT,INSERT,UPDATE,DELETE ON inquisition.* TO inquisition@'localhost'; FLUSH PRIVILEGES"
+    echo "Import table schema..."
+    mysql -u root -p  inquisition < ./src/inquisition.sql || exit 1
+fi
 
 # setup log db
 redis-cli set log_id 0 || echo "COULD NOT CONNECT TO REDIS!" && exit 1
