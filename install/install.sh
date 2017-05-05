8f0a630fd20276cbe3eab4d09724fcffc2b2fce9
Josh Carlson <magneticstain@gmail.com>
2017-05-04 20:35:44 -0400

    Install Script Ammendment - 2

--- install/install.sh
+++ install/install.sh
@@ -6,13 +6,19 @@
 # v1.0
 #
 
-# check if this is a build or not (build don't require MySQL passwords
 BUILD_FLAG=0
+MYSQL_PASS_FLAG=''
+MYSQL_TABLE_SCHEMA_FILE='install/src/inquisition.sql'
+APP_DIR='/opt/inquisition/'
+LOG_DIR='/var/log/inquisition/'
+
+# check if this is a build or not (build don't require MySQL passwords
 while test $# -gt 0
 do
     case "$1" in
         --is-build)
             BUILD_FLAG=1
+            MYSQL_TABLE_SCHEMA_FILE='build/src/inquisition.sql'
             ;;
         *)
             echo "UNKNOWN ARGUMENT :: [ $1 ]"
@@ -20,9 +26,6 @@ do
     esac
     shift
 done
-
-APP_DIR='/opt/inquisition/'
-LOG_DIR='/var/log/inquisition/'
 
 # create directories
 echo "Creating application directory..."
@@ -37,7 +40,6 @@ mkdir $LOG_DIR > /dev/null 2>&1
 rsync -av --exclude 'build' --exclude 'install' --exclude '.travis.yml' ./* $APP_DIR || exit 1
 
 # provision db
-MYSQL_PASS_FLAG=''
 if [ $BUILD_FLAG == 1 ]
 then
     # update directory perms
@@ -62,7 +64,7 @@ mysql -u root $MYSQL_PASS_FLAG -e "CREATE DATABASE inquisition"
 echo "Creating DB service account..."
 mysql -u root $MYSQL_PASS_FLAG -e "CREATE USER inquisition@'localhost' IDENTIFIED BY ''; GRANT SELECT,INSERT,UPDATE,DELETE ON inquisition.* TO inquisition@'localhost'; FLUSH PRIVILEGES"
 echo "Import table schema..."
-mysql -u root $MYSQL_PASS_FLAG inquisition < install/src/inquisition.sql || exit 1
+mysql -u root $MYSQL_PASS_FLAG inquisition < $MYSQL_TABLE_SCHEMA_FILE || exit 1
 
 # setup log db
 redis-cli set log_id 0 || (echo "COULD NOT CONNECT TO REDIS!" && exit 1)
