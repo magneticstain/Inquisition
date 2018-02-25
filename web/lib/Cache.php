72fe2b5b9e15ac562238e541fb7fae387c3387f7
Josh Carlson <magneticstain@gmail.com>
2018-02-25 12:32:11 -0500

    Update configs to reflect fix of config vals in parse_ini_file() in Config()

---
+++ web/lib/Cache.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *  Cache.php - library for caching-related logic
+ */
+
+class Cache
+{
+    public $cachingServerConn = null;
+
+    public function __construct()
+    {
+
+    }
+}
