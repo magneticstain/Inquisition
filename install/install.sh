71f7dc2bcb7e2b698a3f27ca8b20054e47f2fe0c
Josh Carlson <magneticstain@gmail.com>
2018-11-11 14:29:40 -0500

    Update Install Script

--- install/install.sh
+++ install/install.sh
@@ -6,16 +6,37 @@
 # v1.0
 # =======================================
 
+function createServiceAccts()
+{
+    # create service accounts (including matching grp) for applications to run under
+    /usr/sbin/useradd -r inquisition
+}
+
 function createDirStructure()
 {
+    APP_DIR=$1
+    if [ -z "$APP_DIR" ]
+    then
+       APP_DIR='/opt/inquisition/'
+    fi
+    LOG_DIR=$2
+    if [ -z "$LOG_DIR" ]
+    then
+        LOG_DIR='/var/log/inquisition/'
+    fi
+
     # create directories
-    echo "Creating application directory @ [ $1 ]..."
-    mkdir $1 > /dev/null 2>&1
+    echo "Creating application directory @ [ $APP_DIR ]..."
+    mkdir $APP_DIR || echo "[ ERROR ] could not create app directory; try checking permissions"
     echo "Creating application subdirectories..."
     echo "tmp/"
-    mkdir $1'/tmp/' > /dev/null 2>&1
-    echo "Creating log directory @ [ $2 ]..."
-    mkdir $2 > /dev/null 2>&1
+    mkdir $APP_DIR'/tmp/'
+    echo "Creating log directory @ [ $LOG_DIR ]..."
+    mkdir $LOG_DIR || echo "[ ERROR ] could not create log directory; try checking permissions"
+
+    # set perms
+    echo "Setting file permissions..."
+    chown -R inquisition:inquisition $APP_DIR $LOG_DIR || echo '[ ERROR ] could not set file permissions'
 }
 
 function syncAppFiles()
@@ -39,12 +60,8 @@ function runBuildPrep()
     cp -rf build/src/sample_logs/* $TEST_LOG_DIR
 
     # update directory perms
-    chown -R travis $1
-    chmod -R 777 $1
-    chown -R travis $2
-    chmod -R 777 $2
-    chown -R travis $TEST_LOG_DIR
-    chmod -R 777 $TEST_LOG_DIR
+    chown -R travis $1 $2 $TEST_LOG_DIR
+    chmod -R 777 $1 $2 $TEST_LOG_DIR
 }
 
 function initializeInquisitionDb()
@@ -70,50 +87,59 @@ function initializeInquisitionDb()
 
 # MAIN
 BUILD_FLAG=0
+STANDALONE_INSTALL_FLAG=0
 MYSQL_PASS_FLAG=''
 MYSQL_TABLE_SCHEMA_FILE='install/src/inquisition.sql'
 APP_DIR='/opt/inquisition'
 LOG_DIR='/var/log/inquisition'
 
-# check if this is a build or not (build don't require MySQL passwords
+# check if this is a build or not
 while test $# -gt 0
 do
     case "$1" in
         --is-build)
             BUILD_FLAG=1
-            MYSQL_TABLE_SCHEMA_FILE='build/src/inquisition.sql'
+            MYSQL_TABLE_SCHEMA_FILE="build/src/inquisition.sql"
+            ;;
+        -S|--standalone)
+            STANDALONE_INSTALL_FLAG=1
             ;;
         *)
             echo "UNKNOWN ARGUMENT :: [ $1 ]"
             ;;
     esac
+
     shift
 done
 
 echo "[ STARTING INSTALL ]"
 
 # create directories
-createDirStructure $APP_DIR $LOG_DIR
+createDirStructure "$APP_DIR" "$LOG_DIR"
 
 # sync files to app folder
-syncAppFiles $APP_DIR
+syncAppFiles "$APP_DIR"
 
 # run build prep if needed
 if [ $BUILD_FLAG == 1 ]
 then
-    runBuildPrep $APP_DIR $LOG_DIR
+    runBuildPrep "$APP_DIR" "$LOG_DIR"
 else
     # password is needed for accessing db
     MYSQL_PASS_FLAG='-p'
 fi
 
 # init inquisition database
-initializeInquisitionDb $MYSQL_TABLE_SCHEMA_FILE $MYSQL_PASS_FLAG
+if [ $BUILD_FLAG == 1 ] || [ $STANDALONE_INSTALL_FLAG == 1 ]
+then
+    initializeInquisitionDb "$MYSQL_TABLE_SCHEMA_FILE" "$MYSQL_PASS_FLAG"
 
-# setup log db
-redis-cli set log_id 0 || (echo "COULD NOT CONNECT TO REDIS!" && exit 1)
+    # check log db
+    redis-cli set log_id 0 && redis-cli del log_id || (echo "[ ERROR ] could not connect to Redis" && exit 1)
+fi
 
 # copy over logrotate manifest
+echo "Generating log rotation config..."
 /bin/cp -f install/src/inquisition_logrotate_manifest /etc/logrotate.d/inquisition
 
 echo "[ INSTALL COMPLETE! ]"
